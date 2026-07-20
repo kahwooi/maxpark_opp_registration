eca7ac769f6c9a580d2308e3a2e21d14bfafbce5
kahwooi <kahwooi@msn.com>
2026-07-20 22:48:15 +0800

    add form field config

--- models/company.go
+++ models/company.go
@@ -1,24 +1,24 @@
 package models
 
 type CompanyRegisterForm struct {
-	EmployerID             string                 `json:"employerID"`
-	TinNumber              string                 `json:"tinNumber" validate:"min=5,max=16"`
-	CompanyName            string                 `json:"companyName" validate:"required,min=1,max=64"`
-	ContactPerson          string                 `json:"contactPerson" validate:"required,min=5,max=64"`
-	ContactNumber          string                 `json:"contactNumber" validate:"required,min=12,max=12"`
-	ContactEmail           string                 `json:"contactEmail" validate:"required,email,min=5,max=32"`
-	CompanyAddressLine1    string                 `json:"companyAddressLine1" validate:"required,min=5,max=48"`
-	CompanyAddressLine2    string                 `json:"companyAddressLine2" validate:"max=48"`
+	EmployerID             string                 `json:"employerID" validate:"omitempty"`
+	TinNumber              string                 `json:"tinNumber" validate:"omitempty,min=5,max=16"`
+	CompanyName            string                 `json:"companyName" validate:"omitempty,min=1,max=64"`
+	ContactPerson          string                 `json:"contactPerson" validate:"omitempty,min=5,max=64"`
+	ContactNumber          string                 `json:"contactNumber" validate:"omitempty,min=12,max=12"`
+	ContactEmail           string                 `json:"contactEmail" validate:"omitempty,email,min=5,max=32"`
+	CompanyAddressLine1    string                 `json:"companyAddressLine1" validate:"omitempty,min=5,max=48"`
+	CompanyAddressLine2    string                 `json:"companyAddressLine2" validate:"omitempty,max=48"`
 	CompanyPlates          []CompanyPlates        `json:"companyPlates"`
 	CompanySupportingFiles CompanySupportingFiles `json:"companySupportingFiles"`
-	IDType                 string                 `json:"idType" validate:"required"`
-	IDNumber               string                 `json:"idNumber" validate:"required,min=5,max=16"`
-	VehiclePassType        string                 `json:"vehiclePassType"`
+	IDType                 string                 `json:"idType" validate:"omitempty"`
+	IDNumber               string                 `json:"idNumber" validate:"omitempty,min=5,max=16"`
+	VehiclePassType        string                 `json:"vehiclePassType" validate:"omitempty"`
 }
 
 type CompanyPlates struct {
 	NricNumber       string `json:"nricNumber"`
-	PlateNumber      string `json:"plateNumber" validate:"min=1,max=20"`
+	PlateNumber      string `json:"plateNumber" validate:"omitempty,min=1,max=20"`
 	VehicleType      string `json:"vehicleType"`
 	SPAPath          string `json:"spaPath"`
 	ElectricBillPath string `json:"electricBillPath"`
--- models/resident.go
+++ models/resident.go
@@ -1,22 +1,22 @@
 package models
 
 type ResidentRegisterForm struct {
-	ResidentName            string                  `json:"residentName" validate:"required,min=1,max=64"`
-	ContactNumber           string                  `json:"contactNumber" validate:"required,min=1,max=12"`
-	ContactEmail            string                  `json:"contactEmail" validate:"required,email,min=5,max=32"`
-	ResidentAddressLine1    string                  `json:"residentAddressLine1" validate:"required,min=5,max=48"`
-	ResidentAddressLine2    string                  `json:"residentAddressLine2" validate:"max=48"`
+	ResidentName            string                  `json:"residentName" validate:"omitempty,min=1,max=64"`
+	ContactNumber           string                  `json:"contactNumber" validate:"omitempty,min=1,max=12"`
+	ContactEmail            string                  `json:"contactEmail" validate:"omitempty,email,min=5,max=32"`
+	ResidentAddressLine1    string                  `json:"residentAddressLine1" validate:"omitempty,min=5,max=48"`
+	ResidentAddressLine2    string                  `json:"residentAddressLine2" validate:"omitempty,max=48"`
 	TinNumber               string                  `json:"tinNumber" validate:"omitempty,min=1,max=16"`
 	IsTenant                bool                    `json:"isTenant"`
 	ResidentPlates          []ResidentPlates        `json:"residentPlates"`
 	ResidentSupportingFiles ResidentSupportingFiles `json:"residentSupportingFiles"`
-	IDType                  string                  `json:"idType" validate:"required"`
-	IDNumber                string                  `json:"idNumber" validate:"required,min=5,max=16"`
-	VehiclePassType         string                  `json:"vehiclePassType"`
+	IDType                  string                  `json:"idType" validate:"omitempty"`
+	IDNumber                string                  `json:"idNumber" validate:"omitempty,min=5,max=16"`
+	VehiclePassType         string                  `json:"vehiclePassType" validate:"omitempty"`
 }
 
 type ResidentPlates struct {
-	PlateNumber string `json:"plateNumber" validate:"min=1,max=20"`
+	PlateNumber string `json:"plateNumber" validate:"omitempty,min=1,max=20"`
 	VehicleType string `json:"vehicleType"`
 	VehiclePath string `json:"vehiclePath"`
 }
